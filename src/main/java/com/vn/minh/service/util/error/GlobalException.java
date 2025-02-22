bd8f3cafb46485e464f237001b9936519ab82ae8
Your Name <you@example.com>
2025-02-22 23:15:11 +0700

    update

--- src/main/java/com/vn/minh/service/util/error/GlobalException.java
+++ src/main/java/com/vn/minh/service/util/error/GlobalException.java
@@ -9,45 +9,61 @@ import org.springframework.web.servlet.resource.NoResourceFoundException;
 import com.vn.minh.domain.response.ResponseData;
 
 @RestControllerAdvice
+@SuppressWarnings("rawtypes")
+
 public class GlobalException {
 
-    @ExceptionHandler(Exception.class)
-    public ResponseEntity<ResponseData> postMethodName(Exception ex) {
+    private ResponseData createResponseData(int statusCode, String error, String message) {
 
         ResponseData res = new ResponseData();
-        int statusCode = HttpStatus.BAD_GATEWAY.value();
         res.setStatus(statusCode);
-        res.setError(("Một lỗi ngớ ngẩn nào đó chưa fix"));
-        String message = ex.getMessage();
+        res.setError(error);
+
         res.setMessage(message);
         res.setData(null);
+        return res;
+    }
+
+    @SuppressWarnings("unchecked")
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ResponseData> ComonExcetion(Exception ex) {
+
+        int statusCode = HttpStatus.BAD_REQUEST.value();
+        String error = "Một lỗi ngớ ngẩn nào đó chưa fix";
+
+        String message = ex.getMessage();
+
+        ResponseData res = createResponseData(statusCode, error, message);
+
         return ResponseEntity.badRequest().body(res);
     }
 
     @ExceptionHandler(MessageCustomExcetion.class)
-    public ResponseEntity<ResponseData> postMethodName(MessageCustomExcetion ex) {
+    @SuppressWarnings("unchecked")
+    public ResponseEntity<ResponseData> idException(MessageCustomExcetion ex) {
 
-        ResponseData res = new ResponseData();
-        int statusCode = HttpStatus.BAD_GATEWAY.value();
-        res.setStatus(statusCode);
-        res.setError(("Ngoại lệ sai id"));
+        int statusCode = HttpStatus.BAD_REQUEST.value();
+        String error = "Ngoại lệ sai id";
+
         String message = ex.getMessage();
-        res.setMessage(message);
-        res.setData(null);
+
+        ResponseData res = createResponseData(statusCode, error, message);
+
         return ResponseEntity.badRequest().body(res);
     }
 
     @ExceptionHandler(NoResourceFoundException.class)
-    public ResponseEntity<ResponseData> notFoundException(NoResourceFoundException ex) {
+    @SuppressWarnings("unchecked")
 
+    public ResponseEntity<ResponseData> notFoundException(NoResourceFoundException ex) {
         ResponseData res = new ResponseData();
-        int statusCode = HttpStatus.BAD_GATEWAY.value();
+        int statusCode = HttpStatus.NOT_FOUND.value();
         res.setStatus(statusCode);
         res.setError(("Không tìm thấy tài nguyên"));
         String message = ex.getMessage();
         res.setMessage(message);
         res.setData(null);
-        return ResponseEntity.badRequest().body(res);
+        return ResponseEntity.notFound().build();
     }
 
 }
